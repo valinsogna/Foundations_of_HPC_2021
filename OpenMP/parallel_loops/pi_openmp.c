e742708992177c6f5b9b7f37bdc1230cb80c6b5e
Valeria Insogna <valinsogna@gmail.com>
2022-02-13 12:06:40 +0100

    a

--- OpenMP/parallel_loops/pi_openmp.c
+++ OpenMP/parallel_loops/pi_openmp.c
@@ -26,23 +26,26 @@ int main ( int argc, char **argv)
     printf("omp calculation with %d threads\nN=%Ld\n",
 	   nthreads ,N);
 
-    double timing = omp_get_wtime();
+    double timing = omp_get_wtime(); // outside parallel region
     #pragma omp parallel
     {
       int myid = omp_get_thread_num();
       double x, y ;
-      srand48(SEED*(myid+1));
+      srand48(SEED*(myid+1)); //it's thread safe
 
      #pragma omp for reduction(+:M)
       for( long long unsigned i = 0; i < N; i++)
 	{
-	  x = drand48(); //Internal status of entropy miust differ among threads: use erand48, not drand48
+    //man srand48
+    //drand48 it's thread safe, but it's multi-thread unsafe  in sense that it's race!!!!!!NOT a parallel random generator
+	  //These functions have an internal status (a value of the entropy) and in parallel each thread should have a unique status specifically!
+    x = drand48(); //Internal status of entropy miust differ among threads: use erand48, not drand48!
 	  y = drand48();
 	  M += ((x*x + y*y) < 1.0);
 	}
     }
     
-    timing = omp_get_wtime() - timing;
+    timing = omp_get_wtime() - timing; // outside parallel region: walltime
     printf("Estimation of pi: %1.9f\n Walltime:%g\n",
 	   (4.0*(double)M)/N, timing );
       
